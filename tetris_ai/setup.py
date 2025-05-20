69c191216a216cc9da432bdb064150235ec7a578
Andrew Gunyan <andrewgunyan@gmail.com>
2025-05-19 18:50:16 -0600

    First upload to this repository

---
+++ tetris_ai/setup.py
@@ -0,0 +1,17 @@
+GAME_BOARD_WIDTH = 10
+GAME_BOARD_HEIGHT = 20
+
+STATE_INPUT = 'short'
+SEARCH = True
+T_SPIN_MARK = True
+OUTER_MAX = 5
+CPU_MAX = 4
+GAME_TYPE = 'regular'
+
+FOLDER_NAME = './tetris_regular_cnn_v1_oct_27/'
+
+OUT_START = 10
+
+# MODE = 'human_player'
+MODE = 'ai_player_training'
+# MODE = 'ai_player_watching'
