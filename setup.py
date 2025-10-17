9ca9ab764396856e009836afdc4ea8a068511f45
Andrew Gunyan <andrew.gunyan@imsar.com>
2025-10-17 10:21:50 -0600

    Removed dataset to save space

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ GAME_BOARD_HEIGHT = 20
 STATE_INPUT = 'short'
 SEARCH = True
 T_SPIN_MARK = True
-OUTER_MAX = 1
-CPU_MAX = 2
+OUTER_MAX = 25
+CPU_MAX = 5
 GAME_TYPE = 'regular'
 
 FOLDER_NAME = './tetris_regular_cnn_v1_oct_27/'
 
-OUT_START = 10
+OUT_START = 15
 
 # MODE = 'human_player'
-# MODE = 'ai_player_training'
-MODE = 'ai_player_watching'
+MODE = 'ai_player_training'
+# MODE = 'ai_player_watching'
